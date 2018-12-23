5bb73dea58768f49cff9192f9ea59eb79bc271d2
vgupta1192 <vgupta1192@gmail.com>
2018-12-23 18:37:17 +0530

    Services Update for Secure access

--- src/app/currency-converter/currency.service.ts
+++ src/app/currency-converter/currency.service.ts
@@ -10,17 +10,17 @@ export class CurrencyService {
     constructor(private httpClient: HttpClient) {}
     getCurrency(currencyRegion: string): Observable<Currency> {
         if (currencyRegion === 'usd_inr') {
-            return this.httpClient.get<Currency>('http://free.currencyconverterapi.com/api/v5/convert?q=USD_INR&compact=y');
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=USD_INR&compact=y');
         } else if (currencyRegion === 'inr_usd') {
-            return this.httpClient.get<Currency>('http://free.currencyconverterapi.com/api/v5/convert?q=INR_USD&compact=y');
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=INR_USD&compact=y');
         } else if (currencyRegion === 'jpy_inr') {
-            return this.httpClient.get<Currency>('http://free.currencyconverterapi.com/api/v5/convert?q=JPY_INR&compact=y');
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=JPY_INR&compact=y');
         } else if (currencyRegion === 'inr_jpy') {
-            return this.httpClient.get<Currency>('http://free.currencyconverterapi.com/api/v5/convert?q=INR_JPY&compact=y');
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=INR_JPY&compact=y');
         } else if (currencyRegion === 'eur_inr') {
-            return this.httpClient.get<Currency>('http://free.currencyconverterapi.com/api/v5/convert?q=EUR_INR&compact=y');
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=EUR_INR&compact=y');
         } else if (currencyRegion === 'inr_eur') {
-            return this.httpClient.get<Currency>('http://free.currencyconverterapi.com/api/v5/convert?q=INR_EUR&compact=y');
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=INR_EUR&compact=y');
         }
     }
 }
