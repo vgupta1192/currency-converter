88e694b123903bf4064f46356c3fd3f39b20c3fe
vgupta1192 <vgupta1192@gmail.com>
2018-12-24 10:30:20 +0530

    optimized service code

--- src/app/currency-converter/currency.service.ts
+++ src/app/currency-converter/currency.service.ts
@@ -9,18 +9,6 @@ import { Observable } from 'rxjs';
 export class CurrencyService {
     constructor(private httpClient: HttpClient) {}
     getCurrency(currencyRegion: string): Observable<Currency> {
-        if (currencyRegion === 'usd_inr') {
-            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=USD_INR&compact=y');
-        } else if (currencyRegion === 'inr_usd') {
-            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=INR_USD&compact=y');
-        } else if (currencyRegion === 'jpy_inr') {
-            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=JPY_INR&compact=y');
-        } else if (currencyRegion === 'inr_jpy') {
-            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=INR_JPY&compact=y');
-        } else if (currencyRegion === 'eur_inr') {
-            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=EUR_INR&compact=y');
-        } else if (currencyRegion === 'inr_eur') {
-            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=INR_EUR&compact=y');
-        }
+            return this.httpClient.get<Currency>('https://free.currencyconverterapi.com/api/v5/convert?q=' + currencyRegion + '&compact=y');
     }
 }
